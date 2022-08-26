aafe0fed8db76f7a8105083a8f1c5a985fc627b3
Traitawat Jitchana <traitawat.jitchana@accenture.com>
2022-08-26 15:56:53 +0700

    Add uploadImageForQuiz

--- controllers/imageController.js
+++ controllers/imageController.js
@@ -9,7 +9,7 @@ const firestore = firebase.firestore();
 
 const uploadImageForStudent = async (req, res, next) => {
     try {
-        const storage = firebase.storage().ref()
+      
         const file = req.files.studizImg;
         console.log(file);
         const timestamp = Date.now()
@@ -31,7 +31,7 @@ const uploadImageForStudent = async (req, res, next) => {
             var data = dataStudent.data()
             data.imageUrl = imageURL
             await student.update(data);
-            res.send(data)
+            res.status(200).send(data)
 
         
 
@@ -43,7 +43,63 @@ const uploadImageForStudent = async (req, res, next) => {
     }
 }
 
+const uploadImageForQuiz = async (req, res, next) => {
+    try {
+   
+        const file = req.files.studizImg;
+        console.log(file);
+        const timestamp = Date.now()
+        const fileName = `${timestamp}_${file.name}`;
+        console.log(fileName)
+        const imageRef = storage.child(fileName);
+    
+        const snapshot = await imageRef.put(file.data, {
+            contentType: file.mimetype,
+        })
+  
+        const imageURL = await snapshot.ref.getDownloadURL();
+        console.log(imageURL);
+     
+            const id = req.params.id;
+            const no = req.params.no
+            const quiz = await firestore.collection('quizes').doc(id);
+            // console.log(student);
+            const datQuiz = await quiz.get();
+            var data = datQuiz.data()
+            data.questions[no].imageUrl = imageURL
+            await quiz.update(data);
+            res.status(200).send(data)
+
+        
+
+        // res.send(downloadURL);
+        // res.send(pathReference);
+        // res.status(200).send(downloadURL)
+    } catch (error) {
+        res.status(400).send(error.message);
+    }
+}
+
+const deleteImage = async (req, res, next) => {
+    try {
+        const fileUrl = req.body.imageUrl
+        const storage = firebase.storage()
+        var fileRef = storage.refFromURL(fileUrl)
+
+        fileRef.delete().then(function () {
+
+            console.log("File Deleted")
+        }).catch(function (error) {
+
+        });
+        res.status(200).send("delete image succes")
+    } catch (error) {
+        res.status(400).send(error.message);
+    }
+}
+
 module.exports = {
-    // uploadImage,
-    uploadImageForStudent
+    uploadImageForStudent,
+    uploadImageForQuiz,
+    deleteImage
 }
--- routes/image-routes.js
+++ routes/image-routes.js
@@ -1,11 +1,11 @@
 const express = require('express');
-const { uploadImageForStudent } = require('../controllers/imageController');
+const { uploadImageForStudent, uploadImageForQuiz, deleteImage } = require('../controllers/imageController');
 const router = express.Router();
 
 // router.post('/upload/image',uploadImage);
 router.post('/upload/student/image/:id', uploadImageForStudent);
-
-
+router.post('/upload/quiz/image/:id/:no', uploadImageForQuiz);
+router.delete('/delete/image', deleteImage);
 
 module.exports = {
     routes: router
