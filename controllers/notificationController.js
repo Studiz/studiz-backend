c09d50767640620e3fc94b076c91b8bc7d8b1680
Traitawat Jitchana <traitawat.jitchana@accenture.com>
2022-10-25 18:13:47 +0700

    Add getNotificationByStudentId and deleteNotification and readNotification API

--- controllers/notificationController.js
+++ controllers/notificationController.js
@@ -6,7 +6,18 @@ const jwtDecode = require('jwt-decode');
 
 const addNotification = async (req, res, next) => {
     try {
+        try {
+            var decodeToken = jwtDecode(req.headers.token);
+        } catch (error) {
+            return res.status(401).json({
+                "errCode": 401,
+                "errText": "Unauthorized"
+            });
+        }
+
+        const uid = decodeToken.user_id
         const notification = req.body
+        notification.uid = uid
         await firestore.collection('notifications').add(notification);
         res.status(200).send(notification);
     } catch (error) {
@@ -14,17 +25,78 @@ const addNotification = async (req, res, next) => {
     }
 }
 
-const getNotificationById = async (req, res, next) => {
+const getNotificationByStudentId = async (req, res, next) => {
     try {
-        const id = req.params.id;
-        const notification = await firestore.collection('notifications').doc(id);
-        const data = await notification.get();
-
-        if (!data.exists) {
-            res.status(404).send('notification with the given ID not found');
-        } else {
-            res.send(data.data());
+        try {
+            var decodeToken = jwtDecode(req.headers.token);
+        } catch (error) {
+            return res.status(401).json({
+                "errCode": 401,
+                "errText": "Unauthorized"
+            });
         }
+
+        const studentUid = req.params.uid;
+        const notifications = await firestore.collection('notifications');
+        const data = await notifications.get();
+        const notificationsArray = []
+        data.forEach(doc => {
+            if (doc.data().uid === studentUid) {
+                let notification = doc.data()
+                notification.id = doc.id
+                notificationsArray.push(notification);
+            }
+        });
+
+        if (notificationsArray.length === 0) {
+            res.status(404).send('notification with the given studentUid not found');
+        } else {
+            res.send(notificationsArray);
+        }
+    } catch (error) {
+        res.status(400).send(error.message);
+    }
+}
+
+const deleteNotification = async (req, res, next) => {
+    try {
+        try {
+            var decodeToken = jwtDecode(req.headers.token);
+        } catch (error) {
+            return res.status(401).json({
+                "errCode": 401,
+                "errText": "Unauthorized"
+            });
+        }
+
+        const id = req.params.id;
+        await firestore.collection('notifications').doc(id).delete();
+        res.send('notification record deleted successfuly');
+    } catch (error) {
+        res.status(400).send(error.message);
+    }
+}
+
+const readNotification = async (req, res, next) => {
+    try {
+        try {
+            var decodeToken = jwtDecode(req.headers.token);
+        } catch (error) {
+            return res.status(401).json({
+                "errCode": 401,
+                "errText": "Unauthorized"
+            });
+        }
+    
+        const id = req.params.id;
+        const notifications = await firestore.collection('notifications').doc(id);
+        const getnotification = await notifications.get()
+        const notificationData = getnotification.data()
+        if(notificationData) {
+        notificationData.isRead = true
+        await notifications.update(notificationData);
+        res.send('notification record updated successfuly');
+        }else res.status(404).send("notification with the given id not found");
     } catch (error) {
         res.status(400).send(error.message);
     }
@@ -32,5 +104,7 @@ const getNotificationById = async (req, res, next) => {
 
 module.exports = {
     addNotification,
-    getNotificationById,
+    getNotificationByStudentId,
+    readNotification,
+    deleteNotification
 }
--- routes/notification-routes.js
+++ routes/notification-routes.js
@@ -1,10 +1,11 @@
 const express = require('express');
-const { getNotificationById, addNotification } = require('../controllers/notificationController');
+const { getNotificationByStudentId, addNotification, readNotification, deleteNotification } = require('../controllers/notificationController');
 const router = express.Router();
 
-router.get('/get/notification/:id', getNotificationById);
+router.get('/get/notification/studentUid/:uid', getNotificationByStudentId);
 router.post('/add/notification',addNotification);
-
+router.put('/read/notification/:id',readNotification);
+router.delete('/delete/notification/:id',deleteNotification);
 
 module.exports = {
     routes: router
