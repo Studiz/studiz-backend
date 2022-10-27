53f4928ecdd42c38e5300f50c0caebfa05286fa5
Traitawat Jitchana <traitawat.jitchana@accenture.com>
2022-10-27 23:15:47 +0700

    Fix 413

--- index.js
+++ index.js
@@ -20,11 +20,11 @@ const firestore = firebase.firestore();
 
 const app = express();
 
-app.use(express.json());
+app.use(express.json({limit: '50mb'}));
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
-    extended: true
+    extended: true,
 }))
 app.use(fileupload());
 
